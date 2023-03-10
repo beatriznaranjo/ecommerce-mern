4c3207599d6f2e3f47688bf67155795d5d45d52e
Beatriz Naranjo <beanaranjo@gmail.com>
2023-03-09 23:32:37 -0600

    Updated

--- back_end/routes/productRoutes.js
+++ back_end/routes/productRoutes.js
@@ -2,4 +2,8 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/productController");
 
-router.get();
+router.get("/", productController.getAllProducts);
+router.get("/:id", productController.getProductById);
+router.post("/", productController.createProduct);
+
+module.exports = router;
